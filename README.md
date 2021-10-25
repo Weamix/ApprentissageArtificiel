2f43674e9b5b32f762ac2d16ffb7acf2e4eb29d4
Vitse Maxime <maxime.vitse@odaxiom.com>
2021-10-25 11:52:02 +0200

    TP0 : Pizza refacto

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Vitse Maxime (I2L)
 Cours : https://www-lisic.univ-littoral.fr/~teytaud/apprentissage.html
 
 TP0 : https://www-lisic.univ-littoral.fr/~teytaud/files/Cours/Apprentissage/tp0.pdf
+
 TP1 :
--- TP0/main.py
+++ TP0/main.py
@@ -2,10 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-x = np.array([[6], [8], [10], [14], [18]])
-y = [7, 9, 13, 17.5, 18]
 
-if __name__ == '__main__':
+def load_data():
+    x = np.array([[6], [8], [10], [14], [18]])
+    y = [7, 9, 13, 17.5, 18]
+    return x, y
+
+
+def load_data_test():
+    x = np.array([[8], [9], [11], [16], [12]])
+    y = [11, 8.5, 15, 18, 11]
+    return x, y
+
+
+def show_data(x, y):
     f, ax = plt.subplots(1)
     ax.scatter(x, y, color="k")
     ax.set_ylim(ymin=0)
@@ -21,4 +31,16 @@ if __name__ == '__main__':
     points_x = np.arange(0.0, 25.0, 0.1).reshape(-1, 1)
     plt.plot(points_x, reg.predict(points_x))
 
+    print(reg.score(x, y))
+    print(reg.coef_)
+    print(reg.intercept_)
+
     plt.show()
+
+
+if __name__ == '__main__':
+    x, y = load_data()
+    x_test, y_test = load_data_test()
+    show_data(x, y)
+    #show_data(x_test, y_test)
+

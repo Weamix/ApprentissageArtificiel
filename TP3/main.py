fa3e8432e984c19e725db532f9bde9843992e94f
Vitse Maxime <maxime.vitse@decathlon.com>
2021-12-13 15:51:24 +0100

    TP3 tree glass without elaguage

--- TP3/main.py
+++ TP3/main.py
@@ -15,5 +15,21 @@ def tree_barbecue():
     tree.export_graphviz(classifier, out_file='tree.dot', feature_names=['Meteo', 'Amis', 'Vent', 'Jour'])
 
 
+def tree_glass():
+    data = pd.read_csv("glass-data.csv")
+    print(data)
+    del data['Id']
+    del data['refractive index']
+
+
+    x_train = data
+    y_train = data["Type"]
+    del x_train['Type']
+    classifier = tree.DecisionTreeClassifier(criterion='entropy')
+    classifier.fit(x_train, y_train)
+    tree.export_graphviz(classifier, out_file='tree.dot', feature_names=['Sodium', 'Magnesium', 'Aluminium', 'Silicon', 'Potassium', 'Calcium', 'Barium', 'Iron'])
+
+
 if __name__ == '__main__':
-    tree_barbecue()
+    #tree_barbecue()
+    tree_glass()

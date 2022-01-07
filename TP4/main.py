bc988fdbf159c932625d628a3aaa12f31684e413
Vitse Maxime <maxime.vitse@decathlon.com>
2022-01-07 10:30:01 +0100

    Init TP4 + exercice 1 train and test data

---
+++ TP4/main.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def generate_number_between_0_and_1_in_square(x):
+    for i in range(x):
+        x1 = np.random.uniform(0, 1, 2)
+        x2 = np.random.uniform(0, 1, 2)
+        x = x1[0] + x1[1]
+        y = x2[0] + x2[1]
+        point = x + y
+        if (point - 1) > 0:
+            tag = 1
+        else:
+            tag = -1
+        return x, y, tag
+
+
+def write_points_in_file(x):
+    with open("test.txt", "w") as f:
+        for x in range(x):
+            x1, y1, tag1 = generate_number_between_0_and_1_in_square(1)
+            f.write(str(x1) + " " + str(y1) + " " + str(tag1) + "\n")
+    with open("train.txt", "w") as f:
+        for x in range(x):
+            x2, y2, tag2 = generate_number_between_0_and_1_in_square(1)
+            f.write(str(x2) + " " + str(y2) + " " + str(tag2) + "\n")
+
+
+if __name__ == '__main__':
+    write_points_in_file(100)

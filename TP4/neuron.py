d3c06e42c2ef574c204b9b356fbf4b9804777afb
Vitse Maxime <maxime.vitse@decathlon.com>
2022-02-22 22:36:14 +0100

    TP4 uptodate

--- TP4/neuron.py
+++ TP4/neuron.py
@@ -13,33 +13,39 @@ class Neuron:
             self.weights.append(r)
 
     def read_line_file(self, file):
+        l = []
         with open(file, 'r') as file:
-            for line in file:
-                line = line.split()
-            return line
+            lines = file.readlines()
+            for line in lines:
+                line = line.rstrip("\n")
+                x = line.split(" ")
+                l.append(x)
+            return l
 
     def calcul_output_neuron(self, line):
-        theta = (line[0] * self.weights[0] + line[1] * self.weights[1]) - self.bias
-        if theta > 0:
-            y = 1
-        else:
-            y = -1
-        print(y)
-        return y
+        sigma = (float(line[0]) * self.weights[0] + float(line[1]) * self.weights[1]) - self.bias
+        if sigma > 0:
+            return 1
+        return -1
 
-    def update_neuro(self,exemple,line):
-        self.bias = self.bias + LEARNING_STEP * (line[3] - self.output)
-        self.weights[0] = self.weights[0] + LEARNING_STEP * (line[3] - self.output) * line[0]
-        self.weights[1] = self.weights[1] + LEARNING_STEP * (line[3] - self.output) * line[1]
+    def update_neuron(self, line):
+        self.bias = self.bias + LEARNING_STEP * (int(line[2]) - self.output) * -0.5
+        self.weights[0] = self.weights[0] + LEARNING_STEP * (int(line[2]) - self.output) * float(line[0])
+        self.weights[1] = self.weights[1] + LEARNING_STEP * (int(line[2]) - self.output) * float(line[1])
 
 
 if __name__ == '__main__':
-    neuron = Neuron(1)
+    neuron = Neuron()
     for i in range(100):
         nb_errors = 0
-        line = neuron.read_line_file('data.txt')
-        output = neuron.calcul_output_neuron(line)
-        if output != line[3]:
-            neuron.update_neuro(line)
-            nb_errors += 1
-    print(nb_errors)
+        line = neuron.read_line_file('test.txt')
+        line_float = list(np.float_(line))
+        for i in range(100):
+            l = line_float[i]
+            output = neuron.calcul_output_neuron(l)
+            print(l)
+            print(output)
+            if int(output) != int(l[2]):
+                neuron.update_neuron(l)
+                nb_errors += 1
+        print("Nombre d'erreurs : {}".format(nb_errors))

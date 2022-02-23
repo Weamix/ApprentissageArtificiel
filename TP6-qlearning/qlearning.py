5ce77dcb9b61e290957773ad348429e65b237f14
Vitse Maxime <maxime.vitse@decathlon.com>
2022-02-23 14:43:34 +0100

    TP6-qlearning : egreedy up to date

--- TP6-qlearning/qlearning.py
+++ TP6-qlearning/qlearning.py
@@ -31,7 +31,9 @@ def adding_rhums_in_map(m):
     for i in range(NUMBER_RHUM):
         r, c = choose_random_point_in_map()
         if m[r, c] == 10:
-            r, c = choose_random_point_in_map()
+            while m[r, c] != 10:
+                r, c = choose_random_point_in_map()
+                m[r, c] = 2
         else:
             m[r, c] = 2
 
@@ -44,28 +46,49 @@ def choose_random_point_in_map():
 
 def e_greedy(matrix, tresor_position):
     starting_position = choose_random_point_in_map()
-    print("starting_position", starting_position)
-    n = np.random.uniform(0, 1)
+    print("starting_position : ", starting_position)
+    random = np.random.uniform(0, 1)
     number_moved = 0
 
-    while number_moved != 1:
-        if n < 0.9:
+    current_position = starting_position
+
+    while number_moved != 20:
+        neighbors = neighbors_for_position(current_position[0], current_position[1])
+        print("neighbors :", neighbors)
+        values = values_for_neighbors(matrix, neighbors)
+        print("values :", values)
+        if random > 0.9:
             # déplacement vers le meilleur des voisins
-            v = list(find_neighbors(matrix, starting_position[0], starting_position[1]))
-            print("neighbors : ", v)
+            maximum = max(values)
+            print("maximum :", maximum)
+            index = values.index(maximum)
+            print("index :", index)
         else:
-            pass
             # random parmi les voisins possibles
+            random_value = np.random.choice(range(len(neighbors)))
+            print("random_value :", random_value)
+            #print("index :", index)
 
-        number_moved = number_moved + 1
+            index = random_value
+
+        current_position = neighbors[index]
+        print("current_position : ", current_position)
+        number_moved += 1
         if starting_position == tresor_position:
+            print("tresor found")
             break
+    print("number_moved : ", number_moved)
 
 
-def find_neighbors(matrix, x, y):
-    xi = (0, -1, 1) if 0 < x < len(matrix) - 1 else ((0, -1) if x > 0 else (0, 1))
-    yi = (0, -1, 1) if 0 < y < len(matrix[0]) - 1 else ((0, -1) if y > 0 else (0, 1))
-    return islice(starmap((lambda a, b: matrix[x + a][y + b]), product(xi, yi)), 1, None)
+# return neighbors index in matrix for a position
+def neighbors_for_position(x, y):
+    n = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    return [i for i in n if 0 <= i[0] < MATRIX_SIZE and 0 <= i[1] < MATRIX_SIZE]
+
+
+# return values of list of positions in matrix
+def values_for_neighbors(matrix, v):
+    return [matrix[i[0], i[1]] for i in v]
 
 
 def q_learning():

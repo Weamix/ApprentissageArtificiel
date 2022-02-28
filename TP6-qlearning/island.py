fb72641598088e621500697c0604370228ab2927
Vitse Maxime <maxime.vitse@decathlon.com>
2022-02-28 08:57:58 +0100

    TP6-qlearning : refacto and starting q learning

--- TP6-qlearning/island.py
+++ TP6-qlearning/island.py
@@ -1,5 +1,4 @@
 import numpy as np
-from itertools import product, starmap, islice
 
 MATRIX_SIZE = 3
 NUMBER_RHUM = 5
@@ -12,13 +11,7 @@ class Island:
         m = self.generate_matrix(matrix_size)
         self.matrix = m
         self.tresor = np.where(m == 10)
-        self.matrix_size = MATRIX_SIZE
         self.number_rhum = NUMBER_RHUM
-
-    def choose_random_point_in_map(self):
-        random_row = np.random.choice(range(1, MATRIX_SIZE))
-        random_column = np.random.choice(range(1, MATRIX_SIZE))
-        return random_row, random_column
 
     def generate_matrix(self,matrix_size):
         dimension = (matrix_size, matrix_size)
@@ -34,9 +27,22 @@ class Island:
     def adding_rhums_in_map(self, m):
         for i in range(NUMBER_RHUM):
             r, c = self.choose_random_point_in_map()
-            if m[r, c] == 10:
-                while m[r, c] != 10:
-                    r, c = self.choose_random_point_in_map()
-                    m[r, c] = 2
-            else:
-                m[r, c] = 2
+            while m[r, c] == 10:
+                r, c = self.choose_random_point_in_map()
+            m[r, c] = 2
+
+
+    def choose_random_point_in_map(self):
+        random_row = np.random.choice(range(1, MATRIX_SIZE))
+        random_column = np.random.choice(range(1, MATRIX_SIZE))
+        return random_row, random_column
+
+    # return neighbors index in matrix for a position
+    def neighbors_for_position(self, x, y):
+        n = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        return [i for i in n if 0 <= i[0] < MATRIX_SIZE and 0 <= i[1] < MATRIX_SIZE]
+
+    # return values of list of positions in matrix
+    def values_for_neighbors(self, matrix, v):
+        return [matrix[i[0], i[1]] for i in v]
+
--- TP6-qlearning/pirate.py
+++ TP6-qlearning/pirate.py
@@ -1,16 +1,8 @@
 import island
 import numpy as np
+from collections import defaultdict
 
-
-# return neighbors index in matrix for a position
-def neighbors_for_position(x, y):
-    n = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-    return [i for i in n if 0 <= i[0] < island.matrix_size and 0 <= i[1] < island.matrix_size]
-
-
-# return values of list of positions in matrix
-def values_for_neighbors(matrix, v):
-    return [matrix[i[0], i[1]] for i in v]
+GAMMA = 0.9
 
 
 def e_greedy(matrix, tresor_position):
@@ -18,39 +10,45 @@ def e_greedy(matrix, tresor_position):
     print("starting_position : ", starting_position)
     random = np.random.uniform(0, 1)
     number_moved = 0
-
     current_position = starting_position
 
     while number_moved != 20:
-        neighbors = neighbors_for_position(current_position[0], current_position[1])
+        neighbors = island.neighbors_for_position(current_position[0], current_position[1])
         print("neighbors :", neighbors)
-        values = values_for_neighbors(matrix, neighbors)
+        values = island.values_for_neighbors(matrix, neighbors)
         print("values :", values)
         if random > 0.9:
             # déplacement vers le meilleur des voisins
             maximum = max(values)
             print("maximum :", maximum)
             index = values.index(maximum)
-            print("index :", index)
+            print("index best :", index)
         else:
             # random parmi les voisins possibles
             random_value = np.random.choice(range(len(neighbors)))
             print("random_value :", random_value)
-            # print("index :", index)
-
             index = random_value
+            print("index random :", index)
 
         current_position = neighbors[index]
         print("current_position : ", current_position)
         number_moved += 1
+
         if starting_position == tresor_position:
             print("tresor found")
             break
+
     print("number_moved : ", number_moved)
 
 
 def q_learning():
-    pass
+    create_q_table()
+
+
+def create_q_table():
+    Q = defaultdict(lambda: np.zeros(4))
+    print(Q)
+    return Q
 
 
 def monte_carlo():
@@ -61,5 +59,6 @@ if __name__ == '__main__':
     island = island.Island()
     print(island.matrix)
     print("tresor position:", island.tresor)
-    e_greedy(island.matrix, island.tresor)
+    #e_greedy(island.matrix, island.tresor)
+    q_learning()
 

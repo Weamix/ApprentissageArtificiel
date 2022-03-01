6049ce586549edc7871d090fbb765ed5a237494b
Vitse Maxime <maxime.vitse@decathlon.com>
2022-03-01 14:20:31 +0100

    [exam] add correlation matrix data 1

--- EVAL/README.md
+++ EVAL/README.md
@@ -1,20 +1,30 @@
 ### Vitse Maxime M1 I2L
 
-Objectif : apprendre la colonne
+Objectif : apprendre la colonne Z
 
 # How to run code?
 
 # Explications/choix
 
-Analyze data : 
+## Analyze data : 
 
 Champs : dataCCfinal_1 / dataCCfinal_2
 
-Nombre d'exemples : 101 / 17 379
-Nombre de caractéristiques descriptives : 25 / 14
-Nombre d'exemples de chaque classe : 101 / 17 379 mais 0 classes car on est en régression
+    Nombre d'exemples : 101 / 17 379
+
+    Nombre de caractéristiques descriptives : 25 / 14
+
+    Nombre d'exemples de chaque classe : 101 / 17 379 mais 0 classes car on est en régression
+
+Régression : fonction  (Classement sur des valeurs numériques)
 
 Matrice de corélation - dataCCfinal_1
+
+    Suppresion des colonnes A, B et C pour générer la matrice coor
+    A et B ont des valeurs fixes
+    C sont des strings
+
+![imgs/correlation_matrix_data_1.png](imgs/correlation_matrix_data_1.png)
 
 Matrice de corélation - dataCCfinal_2
 
--- EVAL/main.py
+++ EVAL/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 
 def analyze(data, type):
@@ -15,12 +18,44 @@ def analyze(data, type):
     # Stats autour de la colonne "Z" :
     print(data[type].value_counts())
     # Matrice corrélation :
+    matrix_correlation(data)
 
+
+def matrix_correlation(data):
+    averages = data.mean()
+    print(averages)
+
+    sns.set()
+    plt.figure(figsize=(15, 8))
+    plt.hist(data)
+    print(data.shape)
+    sns.heatmap(data.corr(), vmax=0.5, cmap="PiYG")
+    plt.title('Correlation matrix')
+    plt.show()
+
+def label_encode(data, col):
+    # Transforme un type catégorie en entier
+    le = LabelEncoder()
+    # On récupère tous les noms de catégories possibles
+    unique_values = list(data[col].unique())
+    le_fitted = le.fit(unique_values)
+    # On liste l'ensemble des valeurs
+    values = list(data[col].values)
+    # On transforme les catégories en entier
+    values_transformed = le.transform(values)
+    # On fait le remplacement de la colonne dans le dataframe d'origine
+    data[col] = values_transformed
 
 if __name__ == '__main__':
     dataCCfinal_1 = pd.read_csv('csv/dataCCfinal_1.csv')
     dataCCfinal_2 = pd.read_csv('csv/dataCCfinal_2.csv')
+
+    #label_encode(dataCCfinal_1, 'C')
+
+    dataCCfinal_1 = dataCCfinal_1.drop(columns=['A','B','C'], axis=1)
+    #dataCCfinal_2 = dataCCfinal_2.drop(['C'], axis=1)
+
     analyze(dataCCfinal_1, 'Z')
-    analyze(dataCCfinal_2, 'Z')
+    #analyze(dataCCfinal_2, 'Z')
 
 
